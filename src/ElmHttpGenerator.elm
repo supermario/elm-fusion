b504371b3f24c99da6f88ac13bf086bc8f87e5fe
Dillon Kearns <dillon@dillonkearns.com>
2021-11-04 11:03:44 -0700

    Include parameters for all variables in elm/http generator.

--- src/ElmHttpGenerator.elm
+++ src/ElmHttpGenerator.elm
@@ -3,37 +3,59 @@ module ElmHttpGenerator exposing (generate)
 import Elm
 import Elm.Annotation
 import Elm.Gen.Http
+import Elm.Pattern
 import InterpolatedField
 import Request exposing (Request)
 
 
 generate : Request -> Elm.Declaration
 generate request =
+    let
+        referencedVariables : List InterpolatedField.Variable
+        referencedVariables =
+            request.headers
+                |> List.concatMap
+                    (\( key, value ) ->
+                        InterpolatedField.referencedVariables key ++ InterpolatedField.referencedVariables value
+                    )
+    in
     (if List.isEmpty request.headers then
-        \_ ->
-            Elm.Gen.Http.get
-                { url = Elm.string request.url
-                , expect = Elm.Gen.Http.expectJson (\_ -> Elm.value "toMsg") (Elm.value "decoder")
-                }
+        Elm.Gen.Http.get
+            { url = Elm.string request.url
+            , expect = Elm.Gen.Http.expectJson (\_ -> Elm.value "toMsg") (Elm.value "decoder")
+            }
 
      else
-        \_ ->
-            Elm.Gen.Http.request
-                { url = Elm.string request.url
-                , headers =
-                    request.headers
-                        |> List.map
-                            (\( key, value ) ->
-                                Elm.Gen.Http.header
-                                    (InterpolatedField.toElmExpression key)
-                                    (InterpolatedField.toElmExpression value)
-                            )
-                        |> Elm.list
-                , method = request.method |> Request.methodToString |> Elm.string
-                , body = Elm.Gen.Http.emptyBody
-                , timeout = Elm.value "Nothing"
-                , expect = Elm.Gen.Http.expectJson (\_ -> Elm.value "toMsg") (Elm.value "decoder")
-                , tracker = Elm.value "Nothing"
-                }
+        Elm.Gen.Http.request
+            { url = Elm.string request.url
+            , headers =
+                request.headers
+                    |> List.map
+                        (\( key, value ) ->
+                            Elm.Gen.Http.header
+                                (InterpolatedField.toElmExpression key)
+                                (InterpolatedField.toElmExpression value)
+                        )
+                    |> Elm.list
+            , method = request.method |> Request.methodToString |> Elm.string
+            , body = Elm.Gen.Http.emptyBody
+            , timeout = Elm.value "Nothing"
+            , expect = Elm.Gen.Http.expectJson (\_ -> Elm.value "toMsg") (Elm.value "decoder")
+            , tracker = Elm.value "Nothing"
+            }
     )
-        |> Elm.fn "request" ( "toMsg", Elm.Annotation.unit )
+        |> Elm.functionWith "request"
+            (toMsgParam
+                :: List.map
+                    (\variable ->
+                        ( Elm.Annotation.string
+                        , InterpolatedField.toElmVar variable
+                        )
+                    )
+                    referencedVariables
+            )
+
+
+toMsgParam : ( Elm.Annotation.Annotation, Elm.Pattern.Pattern )
+toMsgParam =
+    ( Elm.Annotation.unit, Elm.Pattern.var "toMsg" )
--- tests/ElmHttpGeneratorTest.elm
+++ tests/ElmHttpGeneratorTest.elm
@@ -53,6 +53,33 @@ suite =
         , timeout = Nothing
         , tracker = Nothing
         }"""
+        , test "GET with parameters" <|
+            \() ->
+                { url = "https://example.com"
+                , method = Request.GET
+                , body = Request.Empty
+                , headers =
+                    [ ( "accept-language", "${PREFERRED_LANGUAGE};q=0.9" )
+                    , ( "Referer", "http://www.wikipedia.org/" )
+                    ]
+                }
+                    |> toRequest
+                    |> ElmHttpGenerator.generate
+                    |> Elm.declarationToString
+                    |> Expect.equal
+                        """request toMsg preferredLanguage =
+    Http.request
+        { method = "GET"
+        , headers =
+            [ Http.header "accept-language" (preferredLanguage ++ ";q=0.9")
+            , Http.header "Referer" "http://www.wikipedia.org/"
+            ]
+        , url = "https://example.com"
+        , body = Http.emptyBody
+        , expect = Http.expectJson toMsg decoder
+        , timeout = Nothing
+        , tracker = Nothing
+        }"""
         ]
 
 
